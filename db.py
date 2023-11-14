700c538956271058653a76d69f3fac9c1630981d
Hieu Vuong <tvuong5@dons.usfca.edu>
2023-11-13 18:10:48 -0800

    Add comments and requirements file.

--- db.py
+++ db.py
@@ -2,18 +2,20 @@ import mysql.connector
 from dotenv import load_dotenv
 import os
 
+# Load the environment variables
 load_dotenv()
 
+# Give credentials to the connector
 mydb = mysql.connector.connect(
     host=os.getenv('HOST'),
     user=os.getenv('USERNAME'),
     password=os.getenv('PW')
 )
 
-
+# Create a cursor and execute sql statement
 cursor = mydb.cursor()
-
 cursor.execute("SHOW DATABASES")
 
+# Print out result of the statement
 for db_name in cursor:
     print(db_name)
